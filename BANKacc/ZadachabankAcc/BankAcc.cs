f84922ee499774c20ab3691051464bcce6350d67
Slavena Slavova <106436147+SlavenaS@users.noreply.github.com>
2023-09-28 11:53:25 +0300

    Add files via upload

---
+++ BANKacc/ZadachabankAcc/BankAcc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachabankAcc
+{
+    public class BankAcc
+    {
+        private string id;
+        public string Id 
+        {
+            get { return id; }
+            set{ id = value; }
+        } 
+
+        private string name;
+        public string Name
+        {
+            get { return name; }    
+            set { name = value; }
+        }
+
+        private string status;
+        public string Status
+        {
+          get { return status; }
+          set {  status = value; }
+        }
+
+        private decimal smetka;
+        public decimal Smetka
+        {
+            get { return smetka; }
+            set { smetka = value; }
+        }
+        public BankAcc(string id,string name, string status, decimal smetka)
+        {
+            this.name = name;
+            this.id = id;
+            this.status = status;
+            this.smetka = smetka;
+        }
+
+        public void izhod()
+        {
+            Console.WriteLine($"{this.Name} e s ID-{this.Id}, {this.Status} status i {this.Smetka}lv. ");
+        }
+        public void Inserttp(decimal price)
+        {
+            smetka += price;
+        }
+        public void Removep(decimal rprice)
+        {
+            smetka -= rprice;
+        }
+    }
+}
---
+++ BANKacc/ZadachabankAcc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachabankAcc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi danni");
+            BankAcc obj = new BankAcc(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("insertprice");
+            decimal insertprice = decimal.Parse(Console.ReadLine());
+            obj.Inserttp(insertprice);
+            Console.WriteLine("Vuvedi danni");
+            BankAcc obj2 = new BankAcc(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), decimal.Parse(Console.ReadLine()));
+            obj2.Removep(45);
+            List<BankAcc> list = new List<BankAcc>() { obj, obj2};
+            foreach (var item in list)
+            {
+                item.izhod();
+            }
+            
+        }
+    }
+}
